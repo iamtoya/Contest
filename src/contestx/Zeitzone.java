df54d612e4377afe161a99b27bb3e4fea97abdad
andreas.zander <andreas.zander@s27-pc11.schule.local>
2018-06-01 11:02:44 +0200

    zeitklassen erneuret

--- src/contestx/Zeitzone.java
+++ src/contestx/Zeitzone.java
@@ -1,33 +1,57 @@
 package contestx;
 
 public class Zeitzone {
-private int tz1StartHrs=0;
-private int tz1StartMin=0;
-private int tz1EndHrs=0;
-private int tz1EndMin=0;
-private int tz2StartHrs=0;
-private int tz2StartMin=0;
-private int tz2EndHrs=0;
-private int tz2EndMin=0;
-private int tz3StartHrs=0;
-private int tz3StartMin=0;
-private int tz3EndHrs=0;
-private int tz3EndMin=0;
-
-
-
-public void setTz1StartHrs()
-{
+	private int startHours;
+	private int startMins;
+	private int endHours;
+	private int endMins;
 	
-}
-
-public void setTz2StartHrs()
-{
+	public Zeitzone(int h, int m, int hh, int mm)
+	{
+		startHours=h;
+		startMins=m;
+		endHours=hh;
+		endMins=mm;
+	}
 	
-}
-
-public void setTz3StartHrs()
-{
+	public void setStartHours(int h)
+	{
+		startHours=h;
+	}
 	
-}
+	public void setStartMins(int m)
+	{
+		startMins=m;
+	}
+	
+	public void setEndHours(int h)
+	{
+		endHours=h;
+	}
+	
+	public void setEndMins(int m)
+	{
+		endMins=m;
+	}
+	
+	
+	public int getStartHours()
+	{
+		return startHours;
+	}
+	
+	public int getStartMins()
+	{
+		return startMins;
+	}
+	
+	public int getEndHours()
+	{
+		return endHours;
+	}
+	
+	public int getEndMins()
+	{
+		return endMins;
+	}
 }
