587c0456a039026df67abdac77e208f43f3bfe51
andreas.zander <andreas.zander@s27-pc11.schule.local>
2018-06-06 09:09:41 +0200

    get set debate

--- src/contestx/Debate.java
+++ src/contestx/Debate.java
@@ -12,5 +12,45 @@ public class Debate {
 		zeit = new Zeitzone(0,0,1,0);
 		raum = "";
 	}
-	//Andy
+	
+	
+	
+	
+	
+	
+	
+	
+	public Team getTeamPro() {
+		return teamPro;
+	}
+	
+	public Team getTeamCon() {
+		return teamCon;
+	}
+	
+	public Zeitzone getZeit() {
+		return zeit;
+	}
+	
+	public String getRaum() {
+		return raum;
+	}
+	
+	public void setTeamPro(Team teamPro) {
+		this.teamPro = teamPro;
+	}
+	
+	public void setTeamCon(Team teamCon) {
+		this.teamPro = teamCon;
+	}
+	
+	public void setZeit(Zeitzone zeit) {
+		this.zeit = zeit;
+	}
+	
+	public void setRaum(String raum) {
+		this.raum = raum;
+	}
+	
+	
 }
