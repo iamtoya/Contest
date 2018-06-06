162b33faf43cc40f326e26b3659e8a371d7045e8
andreas.zander <andreas.zander@s27-pc11.schule.local>
2018-06-06 09:21:03 +0200

    get set judge

--- src/contestx/Judge.java
+++ src/contestx/Judge.java
@@ -13,4 +13,56 @@ public class Judge {
 		this.erfahren=erfahren;
 	}
 	
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Schule getSchule() {
+		return schule;
+	}
+	
+	public Boolean getErfahren() {
+		return erfahren;
+	}
+	
+	public Boolean getKannZuZZ1() {
+		return kannZuZeit.get(1);
+	}
+	
+	public Boolean getKannZuZZ2() {
+		return kannZuZeit.get(2);
+	}
+	
+	public Boolean getKannZuZZ3() {
+		return kannZuZeit.get(3);
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setSchule(Schule schule) {
+		this.schule = schule;
+	}
+	
+	public void setErfahren(Boolean erfahren) {
+		this.erfahren = erfahren;
+	}
+	
+	public void setKannZuZZ1(Boolean kann) {
+		this.kannZuZeit.set(1, kann);
+	}
+	
+	public void setKannZuZZ2(Boolean kann) {
+		this.kannZuZeit.set(2, kann);
+	}
+	
+	public void setKannZuZZ3(Boolean kann) {
+		this.kannZuZeit.set(3, kann);
+	}
+	
+	
 }
